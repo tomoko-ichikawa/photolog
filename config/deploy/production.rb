60fcd442320a82f7d1cee3e12701fd353b25589e
tomokoichikawa <takator@yahoo.co.jp>
2019-09-09 22:06:25 +0900

    最終コミット

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,5 @@
-server '18.182.199.174', user: 'app', roles: %w{app db web}
-set :ssh_options, keys: '/Users/ichikawatomoko/.ssh/id_rsa'
+server '18.182.199.174',
+  user: 'app',
+  roles: %w{app db web}
+  set :ssh_options,
+    keys: '/Users/ichikawatomoko/.ssh/id_rsa'
